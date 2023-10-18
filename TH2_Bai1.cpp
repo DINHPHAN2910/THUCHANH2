df11ec0eb3a4cab4f09f9430d48347706179ec1e
DINHPHAN2910 <147588724+DINHPHAN2910@users.noreply.github.com>
2023-10-18 10:17:15 +0700

    Add files via upload

--- TH2_Bai1.cpp
+++ TH2_Bai1.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
+
 void nhapmang(int a[50],int n);
 void xuatmang(int a[50],int n);
 void maxmang(int a[50],int n); 
@@ -7,6 +8,12 @@ void tichmang(int a[50],int n);
 void DemvaTinhTongNT(int a[50],int n);
 void sapxep(int a[50],int n);
 void timx(int a[50],int n);
+void chenx(int a[50], int n );
+void mangthaydoi(int a[50],int n);
+void xoaphantu(int a[50], int n);
+
+//////////////////////////////////////////////////////////
+
 int main()
 { 
 int a[50] ,n ;
@@ -14,15 +21,21 @@ printf("Nhap so phan tu mang: ");
     scanf("%d",&n);
 nhapmang(a,n);
 xuatmang(a,n);
-maxmang(a,n); 
+maxmang(a,n);
 tichmang(a,n);
 timx(a,n);
-//DemvaTinhTongNT(a,n); 
+DemvaTinhTongNT(a,n); 
+chenx(a,n);
+mangthaydoi(a,n);
+xoaphantu(a,n);
+mangthaydoi(a,n);
 sapxep(a,n);
-xuatmang(a,n);
+mangthaydoi(a,n);
 getch; 
 }
-///////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 void nhapmang(int a[50],int n)
 {   
     int i;
@@ -31,7 +44,9 @@ void nhapmang(int a[50],int n)
         scanf("%d",&a[i]);
     }    
 }
-/////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 void xuatmang(int a[50],int n)
 { 
     printf("\nMang vua nhap la:");
@@ -42,7 +57,9 @@ int i;
     }
     getch();
 }
-//////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
 void maxmang(int a[50],int n)
 {
 int MAX = a[0];
@@ -53,8 +70,10 @@ int MAX = a[0];
     }
     printf("\nPhan tu lon nhat trong mang la %d", MAX);
 }
-//////////////////////////////////////////////////////////////////////////////
-void tichmang(int a[50],int n)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+void tichmang(int a[50] , int n )
 {
     int Tich=1; 
     for(int i=0; i<n ; i++)
@@ -63,7 +82,9 @@ void tichmang(int a[50],int n)
     }
     printf("\nTich cac phan tu trong mang la: %d", Tich);
 }
-//////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 void sapxep(int  a[50] ,int n)
 {
 	int i,j,tam;
@@ -75,13 +96,98 @@ void sapxep(int  a[50] ,int n)
 	   a[j]=tam;
 	 } 
  } 
+ 
  ////////////////////////////////////////////////////////////////////////////////////////
+ 
 void timx(int a[50], int n)
 { int x;
-printf("\nNhap phan tu X: ");
-scanf("%d",&x);
+int timthay =0;
+printf("\nNhap phan tu X: ");        scanf("%d",&x);
 for (int i=0;i<n;i++) 
-    if (x=a[i])
-printf("\nX bang gia tri cua phan tu a[%d]:",a[i]);
-else printf("\nGia tri khong ton tai trong mang");
+ 	{
+   	 	if (x==a[i]) 
+   	 	{
+   	 		timthay =1;
+   	 		printf("a[%d] ", i);
+		}
+	}
+	if (timthay)
+	{
+		printf("\n La cac phan tu cua a = x");
+	}
+	else
+	{
+		printf("\nGia tri khong ton tai trong mang");
+	}
 }
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+void chenx(int a[50], int n)
+{
+	int k,x,i;
+	printf("\nNhap gia tri x can chen vao mang : ");   scanf("%d", &x);
+		printf("\nNhap vi tri x can chen vao mang : ");   scanf("%d", &k);
+	for (i=n ; i>k ; i--)
+	{ 
+	a[i] = a[i-1];
+	}
+	a[k] = x;
+	n++;
+/*	printf("Mang sau khi chen gia tri x la ");
+	for (i = 0;i<n;i++)
+	printf("%d",a[i]);   */	
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+void mangthaydoi(int a[50],int n)
+{ 
+    printf("\nMang vua thay doi la:");
+int i;
+    for (i=0;i<n;i++)
+    {
+        printf("%5d",a[i]);
+    }
+    getch();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+void xoaphantu(int a[50], int n)
+{
+	int i,j,k;
+	printf("\nNhap gia tri can xoa k : ",&k);   
+	scanf("%d",&k);
+	for (i=0; i<n; i++)
+	 	if (a[i]==k) 
+	 	{ 
+		 	for (j=i; j<n-1; j++)
+	 		a[j] = a[j+1];
+	 }	  n--; i--;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+void DemvaTinhTongNT(int a[50],int n )
+{ int isPrime(int num);
+int i, count=0, sum=0, num;
+{ if ( num<= 1) 
+return 0;
+}
+for (int i=2; i*i<=num; i++ )
+{ 
+if (num%i == 0)
+ { return 0;
+ }
+ return 1;
+}
+for (int i=0;i<n; i++ ){
+if (isPrime(a[i]))
+{ count++; sum+=a[i];
+}
+}
+printf("\nso luong nguyen to trong mang la : ",count);
+printf("\ntong so nguyen to trong mang: ",sum);
+}
+
